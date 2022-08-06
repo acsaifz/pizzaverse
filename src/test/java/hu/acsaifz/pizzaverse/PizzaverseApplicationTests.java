cac595448bbcfc1ee8b47e5c0c10b9c40b2458e9
acsaifz <angeluszmail@gmail.com>
2022-08-06 11:15:59 +0200

    initial commit

---
+++ src/test/java/hu/acsaifz/pizzaverse/PizzaverseApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.acsaifz.pizzaverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
